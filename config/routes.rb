8a797e81a52bd4893e240e72f95efdcae8812dfc
Sam Gubler <samgubler@Sams-MacBook-Pro.local>
2015-04-16 07:43:02 -0600

    Adding view and tests for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root             'static_pages#home'
 
-  get 'help'    => 'static_pages#help'
+  get     'help'    => 'static_pages#help'
 
-  get 'about'   => 'static_pages#about'
+  get     'about'   => 'static_pages#about'
 
-  get 'contact' => 'static_pages#contact'
+  get     'contact' => 'static_pages#contact'
 
-  get 'signup'  => 'users#new'
+  get     'signup'  => 'users#new'
+
+  get     'login'   => 'sessions#new'
+
+  post    'login'   => 'sessions#create'
+
+  delete  'logout'  => 'sessions#destroy'
 
   resources :users
 
